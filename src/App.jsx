1893bc0baea6cf7faaac472876155c70e00eeb7b
Samyak Jain <samyak@evabharat.ai>
2025-09-28 16:45:38 +0530

    Enhance App component layout by adding minWidth to Box for improved responsiveness.

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Header, Hero, TrustSection } from "./components";
 
 function App() {
   return (
-    <Box sx={{ minHeight: "100vh" }}>
+    <Box sx={{ minHeight: "100vh", minWidth: "100vw" }}>
       <Header />
       <Hero />
       <TrustSection />
